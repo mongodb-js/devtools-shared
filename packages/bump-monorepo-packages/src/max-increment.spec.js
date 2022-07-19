af931f2be1bec1cdf43019009b6bc3f6516ae241
Maurizio Casimirri <maurizio.cas@gmail.com>
2022-07-19 16:23:09 +0200

    feat: distribute bump-packages script

---
+++ packages/bump-monorepo-packages/src/max-increment.spec.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+
+const maxIncrement = require('./max-increment');
+
+describe('maxIncrement', function () {
+  it('returns the correct increment', function () {
+    assert.equal(maxIncrement('patch', 'patch'), 'patch');
+    assert.equal(maxIncrement('minor', 'minor'), 'minor');
+    assert.equal(maxIncrement('major', 'major'), 'major');
+
+    assert.equal(maxIncrement('major', 'patch'), 'major');
+    assert.equal(maxIncrement('patch', 'major'), 'major');
+
+    assert.equal(maxIncrement('major', 'minor'), 'major');
+    assert.equal(maxIncrement('minor', 'major'), 'major');
+
+    assert.equal(maxIncrement('minor', 'patch'), 'minor');
+    assert.equal(maxIncrement('patch', 'minor'), 'minor');
+
+    assert.equal(maxIncrement('patch', null), 'patch');
+    assert.equal(maxIncrement('patch', undefined), 'patch');
+    assert.equal(maxIncrement(null, 'patch'), 'patch');
+    assert.equal(maxIncrement(undefined, 'patch'), 'patch');
+  });
+});
