af2dffbf2716296d7ece7064f27f2a3f7b8a73bf
Anna Henningsen <anna.henningsen@mongodb.com>
2023-07-13 13:06:16 +0200

    feat(mongodb-runner): add ConnectionString getter to MongoCluster (#134)
    Instead of only providing the string through a getter, also provide
    the `ConnectionString` object from `mongodb-connection-string-url`.
    
    This is helpful for consumers which need to modify the connection string
    before connecting to the spawned cluster.

--- packages/mongodb-runner/src/index.ts
+++ packages/mongodb-runner/src/index.ts
@@ -1,3 +1,4 @@
 export { MongoServer, MongoServerOptions } from './mongoserver';
 
 export { MongoCluster, MongoClusterOptions } from './mongocluster';
+export type { ConnectionString } from 'mongodb-connection-string-url';
