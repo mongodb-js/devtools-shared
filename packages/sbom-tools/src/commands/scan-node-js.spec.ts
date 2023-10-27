726d6d60ed15117fa8c8ed2e54b8b57f69dc4158
Sergey Petushkov <petushkov.sergey@gmail.com>
2023-10-27 14:25:36 +0200

    chore(sbom-tools): update Node.js version to test against

--- packages/sbom-tools/src/commands/scan-node-js.spec.ts
+++ packages/sbom-tools/src/commands/scan-node-js.spec.ts
@@ -4,6 +4,18 @@ import { scanNodeJs } from './scan-node-js';
 
 import nock from 'nock';
 import { importFixture } from '../../test/helpers';
+import { writeFile } from 'fs/promises';
+
+async function mockCVEResponse(
+  cveId: string,
+  statusCode = 200,
+  response?: any
+) {
+  nock('https://services.nvd.nist.gov')
+    .get('/rest/json/cves/2.0')
+    .query({ cveId })
+    .reply(statusCode, response ?? (await importFixture(`${cveId}.json`)));
+}
 
 describe('scan-node-js', function () {
   beforeEach(async function () {
@@ -19,15 +31,19 @@ describe('scan-node-js', function () {
       .get('/nodejs/security-wg/main/vuln/core/index.json')
       .reply(200, await importFixture('security-wg-index.json'));
 
-    nock('https://services.nvd.nist.gov')
-      .get('/rest/json/cves/2.0')
-      .query({ cveId: 'CVE-2022-32214' })
-      .reply(200, await importFixture('CVE-2022-32214.json'));
-
-    nock('https://services.nvd.nist.gov')
-      .get('/rest/json/cves/2.0')
-      .query({ cveId: 'CVE-2022-32223' })
-      .reply(200, await importFixture('CVE-2022-32223.json'));
+    // All CVEs from the Node.js 18.14.0 version. If you change the version that
+    // this test is testing against, you'll need to update the CVE snapshots
+    for (const cveId of [
+      'CVE-2023-23918',
+      'CVE-2023-23919',
+      'CVE-2023-23936',
+      'CVE-2023-24807',
+      // This one is overriden in the test itself so that we can provide
+      // different responses
+      // 'CVE-2023-23920',
+    ]) {
+      await mockCVEResponse(cveId);
+    }
   });
 
   afterEach(function () {
@@ -35,22 +51,19 @@ describe('scan-node-js', function () {
   });
 
   it('report vulnerabilities for a version of node.js', async function () {
-    nock('https://services.nvd.nist.gov')
-      .get('/rest/json/cves/2.0')
-      .query({ cveId: 'CVE-2022-32222' })
-      .reply(200, await importFixture('CVE-2022-32222.json'));
+    await mockCVEResponse('CVE-2023-23920');
 
-    const result = await scanNodeJs({ version: '16.19.1' });
+    const result = await scanNodeJs({ version: '18.14.0' });
+    await writeFile('expect-ok.json', JSON.stringify(result));
     expect(result).to.deep.equal(await importFixture('expect-ok.json'));
   });
 
-  it('sets the severity to unknown if fetch from nist fails', async function () {
-    nock('https://services.nvd.nist.gov')
-      .get('/rest/json/cves/2.0')
-      .query({ cveId: 'CVE-2022-32222' })
-      .reply(500, 'Server error');
+  it.only('sets the severity to unknown if fetch from nist fails', async function () {
+    await mockCVEResponse('CVE-2023-23920', 500, 'Server error');
 
-    const result = await scanNodeJs({ version: '16.19.1' });
+    const result = await scanNodeJs({ version: '18.14.0' });
+
+    await writeFile('expect-ok.json', JSON.stringify(result));
 
     expect(JSON.parse(JSON.stringify(result, null, 2))).to.deep.equal(
       await importFixture('expect-unknown.json')
