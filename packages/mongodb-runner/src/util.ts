ad987dac40e617e7c42f645d27635058c00c32d7
Bailey Pearson <bailey.pearson@mongodb.com>
2023-11-28 08:22:05 -0700

    add comment and return type to `parallelForEach`

--- packages/mongodb-runner/src/util.ts
+++ packages/mongodb-runner/src/util.ts
@@ -6,10 +6,21 @@ export const uuid = () => new BSON.UUID().toHexString(true);
 export const sleep = (ms: number): Promise<void> =>
   new Promise((r) => setTimeout(r, ms));
 export const range = (n: number): number[] => [...Array(n).keys()];
+
+/**
+ * This function iterates `iterable` and applies `fn` to each item that
+ * `iterable` produces.  `fn` is not awaited on each iteration of `iterable`.
+ * After `iterable` has been consumed, all `fn`s are awaited together with `Promise.allSettled`
+ * and the result of `Promise.allSettled` is returned.
+ *
+ * **Note** This means that any errors from `fn` are caught and returned, not thrown as a rejection.
+ *
+ * This function speeds up the application of `fn` for scenarios where `fn` might take time.
+ */
 export async function parallelForEach<T>(
   iterable: AsyncIterable<T>,
   fn: (arg0: T) => Promise<void> | void
-) {
+): Promise<PromiseSettledResult<void>[]> {
   const result = [];
   for await (const item of iterable) {
     result.push(fn(item));
