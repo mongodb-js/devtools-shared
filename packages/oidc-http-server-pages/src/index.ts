f9398ee46ba2487357a15a411db1146a1f6c3a3a
Paula Stachova <paula.stachova@tutanota.com>
2024-02-15 14:31:41 +0100

    chore(oidc-http-server-pages): import from https://github.com/mongodb-js/devtools-connect/tree/main/src/oidc/static

---
+++ packages/oidc-http-server-pages/src/index.ts
@@ -0,0 +1,7 @@
+export { generateStaticPagesModule } from './create-static-pages';
+export {
+  OIDCPageBaseProps,
+  OIDCAcceptedPage,
+  OIDCNotFoundPage,
+  OIDCErrorPage,
+} from './pages-source';
