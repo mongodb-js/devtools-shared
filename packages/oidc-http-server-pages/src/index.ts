89494db196ee414bdb5f26c16b28d57fa62adeab
Paula Stachova <paula.stachova@tutanota.com>
2024-02-16 16:44:14 +0100

    update dependencies and reduce exports

--- packages/oidc-http-server-pages/src/index.ts
+++ packages/oidc-http-server-pages/src/index.ts
@@ -1,7 +1,1 @@
 export { generateStaticPagesModule } from './create-static-pages';
-export {
-  OIDCPageBaseProps,
-  OIDCAcceptedPage,
-  OIDCNotFoundPage,
-  OIDCErrorPage,
-} from './pages-source';
