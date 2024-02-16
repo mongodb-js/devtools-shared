faeb210472291fa771d9808260e41515784833f9
Paula Stachova <paula.stachova@tutanota.com>
2024-02-16 16:53:26 +0100

    fix

--- packages/oidc-http-server-pages/src/index.ts
+++ packages/oidc-http-server-pages/src/index.ts
@@ -1,1 +1,7 @@
 export { generateStaticPagesModule } from './create-static-pages';
+export {
+  OIDCPageBaseProps,
+  OIDCAcceptedPage,
+  OIDCNotFoundPage,
+  OIDCErrorPage,
+} from './static-pages';
