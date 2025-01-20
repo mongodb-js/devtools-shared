1a9fdee9a8d697319df9b82c121299789ebcc9cc
Gagik Amaryan <me@gagik.co>
2025-01-20 12:52:16 +0100

    feat(mongodb-log-writer): move mongodb-log-writer into devtools-shared MONGOSH-1991 (#498)
    This also includes some refactoring to split the modules of MongoLogManager and MongoLogWriter and export them separately as well as a minor breaking change capitalization typo fix.

---
+++ packages/mongodb-log-writer/src/mongo-log-manager.ts
@@ -0,0 +1,147 @@
+import path from 'path';
+import { ObjectId } from 'bson';
+import { once } from 'events';
+import { createWriteStream, promises as fs } from 'fs';
+import { createGzip, constants as zlibConstants } from 'zlib';
+import { Heap } from 'heap-js';
+import { MongoLogWriter } from './mongo-log-writer';
+import { Writable } from 'stream';
+
+/** Options used by MongoLogManager instances. */
+interface MongoLogOptions {
+  /** A base directory in which log files are stored. */
+  directory: string;
+  /** Whether to write files as .gz files or not. */
+  gzip?: boolean;
+  /** The number of calendar days after which old log files are deleted. */
+  retentionDays: number;
+  /** The maximal number of log files which are kept. */
+  maxLogFileCount?: number;
+  /** A handler for warnings related to a specific filesystem path. */
+  onerror: (err: Error, path: string) => unknown | Promise<void>;
+  /** A handler for errors related to a specific filesystem path. */
+  onwarn: (err: Error, path: string) => unknown | Promise<void>;
+}
+
+/**
+ * A manger for the log files of an application.
+ * Log files will be stored in a single directory, following the
+ * naming convention `${logId}_log`.
+ */
+export class MongoLogManager {
+  _options: MongoLogOptions;
+
+  constructor(options: MongoLogOptions) {
+    this._options = options;
+  }
+
+  /** Clean up log files older than `retentionDays`. */
+  async cleanupOldLogFiles(maxDurationMs = 5_000): Promise<void> {
+    const dir = this._options.directory;
+    let dirHandle;
+    try {
+      dirHandle = await fs.opendir(dir);
+    } catch {
+      return;
+    }
+
+    const deletionStartTimestamp = Date.now();
+    // Delete files older than N days
+    const deletionCutoffTimestamp =
+      deletionStartTimestamp - this._options.retentionDays * 86400 * 1000;
+    // Store the known set of least recent files in a heap in order to be able to
+    // delete all but the most recent N files.
+    const leastRecentFileHeap = new Heap<{
+      fileTimestamp: number;
+      fullPath: string;
+    }>((a, b) => a.fileTimestamp - b.fileTimestamp);
+
+    for await (const dirent of dirHandle) {
+      // Cap the overall time spent inside this function. Consider situations like
+      // a large number of machines using a shared network-mounted $HOME directory
+      // where lots and lots of log files end up and filesystem operations happen
+      // with network latency.
+      if (Date.now() - deletionStartTimestamp > maxDurationMs) break;
+
+      if (!dirent.isFile()) continue;
+      const { id } =
+        /^(?<id>[a-f0-9]{24})_log(\.gz)?$/i.exec(dirent.name)?.groups ?? {};
+      if (!id) continue;
+      const fileTimestamp = +new ObjectId(id).getTimestamp();
+      const fullPath = path.join(dir, dirent.name);
+      let toDelete: string | undefined;
+
+      // If the file is older than expected, delete it. If the file is recent,
+      // add it to the list of seen files, and if that list is too large, remove
+      // the least recent file we've seen so far.
+      if (fileTimestamp < deletionCutoffTimestamp) {
+        toDelete = fullPath;
+      } else if (this._options.maxLogFileCount) {
+        leastRecentFileHeap.push({ fullPath, fileTimestamp });
+        if (leastRecentFileHeap.size() > this._options.maxLogFileCount) {
+          toDelete = leastRecentFileHeap.pop()?.fullPath;
+        }
+      }
+
+      if (!toDelete) continue;
+      try {
+        await fs.unlink(toDelete);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (err: any) {
+        if (err?.code !== 'ENOENT') {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          this._options.onerror(err, fullPath);
+        }
+      }
+    }
+  }
+
+  /** Create a MongoLogWriter stream for a new log file. */
+  async createLogWriter(): Promise<MongoLogWriter> {
+    const logId = new ObjectId().toString();
+    const doGzip = !!this._options.gzip;
+    const logFilePath = path.join(
+      this._options.directory,
+      `${logId}_log${doGzip ? '.gz' : ''}`
+    );
+
+    let originalTarget: Writable;
+    let stream: Writable;
+    let logWriter: MongoLogWriter | undefined;
+    try {
+      stream = createWriteStream(logFilePath, { mode: 0o600 });
+      originalTarget = stream;
+      await once(stream, 'ready');
+      if (doGzip) {
+        stream = createGzip({
+          flush: zlibConstants.Z_SYNC_FLUSH,
+          level: zlibConstants.Z_MAX_LEVEL,
+        });
+        stream.pipe(originalTarget);
+      } else {
+        stream.on('finish', () => stream.emit('log-finish'));
+      }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      this._options.onwarn(err, logFilePath);
+      stream = new Writable({
+        write(chunk, enc, cb) {
+          // Just ignore log data if there was an error.
+          cb();
+        },
+      });
+      originalTarget = stream;
+      logWriter = new MongoLogWriter(logId, null, stream);
+    }
+    if (!logWriter) {
+      logWriter = new MongoLogWriter(logId, logFilePath, stream);
+    }
+
+    // We use 'log-finish' to give consumers an event that they can
+    // listen on which is always only emitted once data has actually
+    // been written to disk.
+    originalTarget.on('finish', () => logWriter?.emit('log-finish'));
+    return logWriter;
+  }
+}
---
+++ packages/mongodb-log-writer/src/mongo-log-writer.spec.ts
@@ -0,0 +1,180 @@
+import type { MongoLogEntry } from '.';
+import { MongoLogWriter, mongoLogId } from '.';
+import { EJSON } from 'bson';
+import stream from 'stream';
+import { inspect } from 'util';
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+
+describe('MongoLogWriter', function () {
+  it('allows writing log messages to a stream', async function () {
+    const now = new Date(1628591965386);
+    const target = new stream.PassThrough().setEncoding('utf8');
+    const writer = new MongoLogWriter('logid', null, target, () => now);
+    const logEvents: MongoLogEntry[] = [];
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    writer.on('log', (entry) => logEvents.push(entry));
+    expect(writer.target).to.equal(target);
+    writer.info('component', mongoLogId(12345), 'context', 'message', {
+      foo: 'bar',
+    });
+    writer.warn('component', mongoLogId(12345), 'context', 'message', {
+      foo: 'bar',
+    });
+    writer.error('component', mongoLogId(12345), 'context', 'message', {
+      foo: 'bar',
+    });
+    writer.fatal('component', mongoLogId(12345), 'context', 'message', {
+      foo: 'bar',
+    });
+    writer.debug(
+      'component',
+      mongoLogId(12345),
+      'context',
+      'message',
+      { foo: 'bar' },
+      2
+    );
+    writer.write({
+      t: now,
+      s: 'E',
+      c: 'x',
+      id: mongoLogId(0),
+      ctx: 'y',
+      msg: 'z',
+    });
+    await writer.flush();
+    const log = target
+      .read()
+      .split('\n')
+      .filter(Boolean)
+      .map((line: string) => JSON.parse(line));
+    expect(log).to.deep.equal(EJSON.serialize(logEvents));
+    expect(log).to.deep.equal([
+      {
+        t: { $date: '2021-08-10T10:39:25.386Z' },
+        s: 'I',
+        c: 'component',
+        id: 12345,
+        ctx: 'context',
+        msg: 'message',
+        attr: { foo: 'bar' },
+      },
+      {
+        t: { $date: '2021-08-10T10:39:25.386Z' },
+        s: 'W',
+        c: 'component',
+        id: 12345,
+        ctx: 'context',
+        msg: 'message',
+        attr: { foo: 'bar' },
+      },
+      {
+        t: { $date: '2021-08-10T10:39:25.386Z' },
+        s: 'E',
+        c: 'component',
+        id: 12345,
+        ctx: 'context',
+        msg: 'message',
+        attr: { foo: 'bar' },
+      },
+      {
+        t: { $date: '2021-08-10T10:39:25.386Z' },
+        s: 'F',
+        c: 'component',
+        id: 12345,
+        ctx: 'context',
+        msg: 'message',
+        attr: { foo: 'bar' },
+      },
+      {
+        t: { $date: '2021-08-10T10:39:25.386Z' },
+        s: 'D2',
+        c: 'component',
+        id: 12345,
+        ctx: 'context',
+        msg: 'message',
+        attr: { foo: 'bar' },
+      },
+      {
+        t: { $date: '2021-08-10T10:39:25.386Z' },
+        s: 'E',
+        c: 'x',
+        id: 0,
+        ctx: 'y',
+        msg: 'z',
+      },
+    ]);
+  });
+
+  it('can log error object as data as-is', async function () {
+    const now = new Date(1628591965386);
+    const target = new stream.PassThrough().setEncoding('utf8');
+    const writer = new MongoLogWriter('logid', null, target, () => now);
+    writer.error(
+      'component',
+      mongoLogId(12345),
+      'context',
+      'message',
+      new Error('foo')
+    );
+    await writer.flush();
+    const log = target
+      .read()
+      .split('\n')
+      .filter(Boolean)
+      .map((line: string) => JSON.parse(line));
+    log[0].attr.stack = '';
+    expect(log[0].attr).to.deep.equal({
+      code: null,
+      message: 'foo',
+      name: 'Error',
+      stack: '',
+    });
+  });
+
+  it('can log non-trivial data', async function () {
+    const now = new Date(1628591965386);
+    const target = new stream.PassThrough().setEncoding('utf8');
+    const writer = new MongoLogWriter('logid', null, target, () => now);
+
+    const cyclic: any = {};
+    cyclic.cyclic = cyclic;
+    writer.error('component', mongoLogId(12345), 'context', 'message', cyclic);
+
+    await writer.flush();
+    const log = target
+      .read()
+      .split('\n')
+      .filter(Boolean)
+      .map((line: string) => JSON.parse(line).attr);
+    expect(log).to.deep.equal([
+      {
+        _inspected: inspect(cyclic),
+      },
+    ]);
+  });
+
+  it('rejects invalid messages', async function () {
+    const errors: Error[] = [];
+    function tryWrite(input: any) {
+      const target = new stream.PassThrough().setEncoding('utf8');
+      const writer = new MongoLogWriter('logid', null, target);
+      writer.on('error', (err) => errors.push(err));
+      writer.write(input);
+    }
+    tryWrite({});
+    tryWrite({ s: 'E' });
+    tryWrite({ s: 'E', c: '' });
+    tryWrite({ s: 'E', c: '', id: mongoLogId(0) });
+    tryWrite({ s: 'E', c: '', id: mongoLogId(0), ctx: '' });
+    tryWrite({ s: 'E', c: '', id: mongoLogId(0), ctx: '', msg: '' });
+
+    await new Promise(setImmediate);
+    expect(errors).to.have.lengthOf(5);
+    expect(new Set([...errors.map((err) => err.name)])).to.deep.equal(
+      new Set(['TypeError'])
+    );
+  });
+});
