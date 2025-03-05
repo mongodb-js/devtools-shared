4848819666d6af41f74393046f3b48ce8c9871f6
Kræn Hansen <kraen.hansen@mongodb.com>
2025-03-05 14:12:17 +0100

    Refactor linux implementation and tests

--- packages/get-os-info/src/get-os-info.spec.ts
+++ packages/get-os-info/src/get-os-info.spec.ts
@@ -2,16 +2,11 @@ import { expect } from 'chai';
 import os from 'os';
 import { promises as fs } from 'fs';
 
-import { getOsInfo } from './get-os-info';
+import { getOsInfo, parseDarwinInfo, parseLinuxInfo } from './get-os-info';
 
 describe('get-os-info', function () {
-  let osInfo;
-  beforeEach(async function () {
-    osInfo = await getOsInfo();
-  });
-
-  it('returns info from "os" module', function () {
-    const { os_arch, os_type, os_version, os_release } = osInfo;
+  it('returns info from "os" module', async function () {
+    const { os_arch, os_type, os_version, os_release } = await getOsInfo();
     expect({ os_arch, os_type, os_version, os_release }).to.deep.equal({
       os_arch: os.arch(),
       os_type: os.type(),
@@ -21,13 +16,31 @@ describe('get-os-info', function () {
   });
 
   describe('on linux', function () {
-    beforeEach(function () {
-      if (process.platform !== 'linux') {
-        this.skip();
-      }
+    it('parses os-release file', function () {
+      // Copied from https://manpages.ubuntu.com/manpages/focal/man5/os-release.5.html#example
+      const fixture = `
+        NAME=Fedora
+        VERSION="17 (Beefy Miracle)"
+        ID=fedora
+        VERSION_ID=17
+        PRETTY_NAME="Fedora 17 (Beefy Miracle)"
+        ANSI_COLOR="0;34"
+        CPE_NAME="cpe:/o:fedoraproject:fedora:17"
+        HOME_URL="https://fedoraproject.org/"
+        BUG_REPORT_URL="https://bugzilla.redhat.com/"
+      `;
+
+      expect(parseLinuxInfo(fixture)).to.deep.equal({
+        os_linux_dist: 'fedora',
+        os_linux_release: '17',
+      });
     });
 
     it('returns info from /etc/releases', async function () {
+      if (process.platform !== 'linux') {
+        this.skip();
+      }
+
       const etcRelease = await fs.readFile('/etc/os-release', 'utf-8');
 
       const releaseKv = etcRelease
@@ -47,7 +60,7 @@ describe('get-os-info', function () {
       expect(distroId).to.match(/^(rhel|ubuntu|debian)$/);
       expect(distroVer).to.match(/^\d+/);
 
-      const { os_linux_dist, os_linux_release } = osInfo;
+      const { os_linux_dist, os_linux_release } = await getOsInfo();
       expect({ os_linux_dist, os_linux_release }).to.deep.equal({
         os_linux_dist: distroId,
         os_linux_release: distroVer,
--- packages/get-os-info/src/get-os-info.ts
+++ packages/get-os-info/src/get-os-info.ts
@@ -1,46 +1,44 @@
 import os from 'os';
 import { promises as fs } from 'fs';
 
+type LinuxInfo = {
+  os_linux_dist: string;
+  os_linux_release: string;
+};
+
 type OsInfo = {
   os_type: string;
   os_version: string;
   os_arch: string;
   os_release: string;
-  os_linux_dist?: string;
-  os_linux_release?: string;
-};
+} & Partial<LinuxInfo>;
 
-async function getLinuxInfo(): Promise<{
-  os_linux_dist: string;
-  os_linux_release: string;
-}> {
+async function getLinuxInfo(): Promise<LinuxInfo> {
   try {
     const releaseFile = '/etc/os-release';
     const etcRelease = await fs.readFile(releaseFile, 'utf-8');
-
-    const osReleaseEntries = etcRelease
-      .split('\n')
-      .map((l) => l.trim())
-      .filter(Boolean)
-      .map((l) => l.split('='))
-      .map(([k, v]) => [
-        k,
-        (v || '').replace(/^["']/, '').replace(/["']$/, ''),
-      ]);
-
-    const osReleaseKv = Object.fromEntries(osReleaseEntries);
-
-    return {
-      os_linux_dist: osReleaseKv.ID || 'unknown',
-      os_linux_release: osReleaseKv.VERSION_ID || 'unknown',
-    };
+    return parseLinuxInfo(etcRelease);
   } catch (e) {
-    // couldn't read /etc/os-release
+    return {
+      os_linux_dist: 'unknown',
+      os_linux_release: 'unknown',
+    };
   }
+}
+
+export function parseLinuxInfo(etcRelease: string): LinuxInfo {
+  const osReleaseEntries = etcRelease
+    .split('\n')
+    .map((l) => l.trim())
+    .filter(Boolean)
+    .map((l) => l.split('='))
+    .map(([k, v]) => [k, (v || '').replace(/^["']/, '').replace(/["']$/, '')]);
+
+  const osReleaseKv = Object.fromEntries(osReleaseEntries);
 
   return {
-    os_linux_dist: 'unknown',
-    os_linux_release: 'unknown',
+    os_linux_dist: osReleaseKv.ID || 'unknown',
+    os_linux_release: osReleaseKv.VERSION_ID || 'unknown',
   };
 }
 
