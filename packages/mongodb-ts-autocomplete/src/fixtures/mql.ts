07b36ccb262fbf911f7dcd5642b91018dc619bcf
Le Roux Bodenstein <lerouxb@gmail.com>
2025-03-21 15:46:17 +0000

    stale comment

--- packages/mongodb-ts-autocomplete/src/fixtures/mql.ts
+++ packages/mongodb-ts-autocomplete/src/fixtures/mql.ts
@@ -1,7 +1,6 @@
 // TODO: everything in here is just a very minimal stub until we can generate
 // this file from the work in MONGOSH-2030
 
-// TODO: most of these are actually MQL
 export interface Document {
   [key: string]: any;
 }
