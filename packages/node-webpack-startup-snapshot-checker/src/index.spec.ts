4cf64a578ebfefba888ad0f0445d42f1ab6dbadf
Nikola Irinchev <irinchev@me.com>
2025-03-29 00:47:25 +0200

    Disable proxy test on linux; use cross-spawn on Windows

--- packages/node-webpack-startup-snapshot-checker/src/index.spec.ts
+++ packages/node-webpack-startup-snapshot-checker/src/index.spec.ts
@@ -1,11 +1,38 @@
-/* eslint-disable no-console */
-import { execFile as execFileCallback } from 'child_process';
-import { promisify } from 'util';
+import { spawn } from 'cross-spawn';
 import path from 'path';
 import { expect } from 'chai';
-const execFile = promisify(execFileCallback);
 
 describe('startup snapshot checker', function () {
+  const execFile = (
+    command: string,
+    args: ReadonlyArray<string>
+  ): Promise<void> => {
+    const proc = spawn(command, args);
+    return new Promise<void>((resolve, reject) => {
+      proc.stderr.setEncoding('utf8');
+      let errBuffer = '';
+      proc.stderr.on('data', (data) => {
+        errBuffer += data;
+      });
+
+      proc.once('exit', (code, signal) => {
+        if (code === 0 && !signal) {
+          resolve();
+        } else {
+          let err: Error;
+          if (signal) {
+            err = new Error(`Failed with signal ${signal}`);
+          } else {
+            err = new Error(`Failed with code ${code}`);
+          }
+
+          err.message += '\n' + errBuffer;
+          reject(err);
+        }
+      });
+    });
+  };
+
   it('can detect when a package is simple enough to be snapshotted', async function () {
     await execFile('npx', [
       'ts-node',
@@ -13,6 +40,7 @@ describe('startup snapshot checker', function () {
       path.resolve(__dirname, '..', 'test', 'fixtures', 'snapshottable.js'),
     ]);
   });
+
   it('can detect when a package is too complex to be snapshotted', async function () {
     try {
       await execFile('npx', [
@@ -20,6 +48,7 @@ describe('startup snapshot checker', function () {
         path.resolve(__dirname, 'index.ts'),
         path.resolve(__dirname, '..', 'test', 'fixtures', 'unsnapshottable.js'),
       ]);
+
       expect.fail('missed exception');
     } catch (err) {
       expect(err).to.match(/Command failed/);
