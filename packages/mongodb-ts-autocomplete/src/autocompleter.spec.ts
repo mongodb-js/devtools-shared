d575606f35d8fada4c5a10164e06f04c59256e25
Le Roux Bodenstein <lerouxb@gmail.com>
2025-07-07 12:50:20 +0100

    suggest collections with dots in them

--- packages/mongodb-ts-autocomplete/src/autocompleter.spec.ts
+++ packages/mongodb-ts-autocomplete/src/autocompleter.spec.ts
@@ -84,7 +84,7 @@ describe('MongoDBAutocompleter', function () {
       databasesForConnection: () =>
         Promise.resolve(['db1', 'db2', databaseName]),
       collectionsForDatabase: () =>
-        Promise.resolve(['foo', 'bar', 'baz', collectionName]),
+        Promise.resolve(['foo', 'bar', 'baz', collectionName, 'one.two']),
       schemaInformationForCollection: async (
         connectionId: string,
         databaseName: string,
@@ -212,6 +212,19 @@ describe('MongoDBAutocompleter', function () {
           kind: 'property',
           name: 'foo',
           result: 'db.foo',
+        },
+      ]);
+    }
+  });
+
+  it('completes a collection name with a dot in it', async function () {
+    for (let i = 0; i < 2; i++) {
+      const completions = await autocompleter.autocomplete('db.on');
+      expect(completions).to.deep.equal([
+        {
+          kind: 'property',
+          name: 'one.two',
+          result: 'db.one.two',
         },
       ]);
     }
