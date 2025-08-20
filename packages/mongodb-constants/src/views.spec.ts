3ba08d8e8aa7753bd33c127366001e444d954058
Darshana Venkatesh <darshana.venkatesh@mongodb.com>
2025-08-20 16:01:00 -0700

    update view.spec file

--- packages/mongodb-constants/src/views.spec.ts
+++ packages/mongodb-constants/src/views.spec.ts
@@ -1,38 +1,44 @@
 import { expect } from 'chai';
-import {
-  isPipelineSearchQueryable,
-  isVersionSearchCompatibleForViewsCompass,
-  isVersionSearchCompatibleForViewsDataExplorer,
-} from './views';
+import { VIEW_PIPELINE_UTILS } from './views';
 import type { Document } from 'mongodb';
 
 describe('views', function () {
   describe('isPipelineSearchQueryable', function () {
     it('should return true for a valid pipeline with $addFields stage', function () {
       const pipeline: Document[] = [{ $addFields: { testField: 'testValue' } }];
-      expect(isPipelineSearchQueryable(pipeline)).to.equal(true);
+      expect(VIEW_PIPELINE_UTILS.isPipelineSearchQueryable(pipeline)).to.equal(
+        true,
+      );
     });
 
     it('should return true for a valid pipeline with $set stage', function () {
       const pipeline: Document[] = [{ $set: { testField: 'testValue' } }];
-      expect(isPipelineSearchQueryable(pipeline)).to.equal(true);
+      expect(VIEW_PIPELINE_UTILS.isPipelineSearchQueryable(pipeline)).to.equal(
+        true,
+      );
     });
 
     it('should return true for a valid pipeline with $match stage using $expr', function () {
       const pipeline: Document[] = [
         { $match: { $expr: { $eq: ['$field', 'value'] } } },
       ];
-      expect(isPipelineSearchQueryable(pipeline)).to.equal(true);
+      expect(VIEW_PIPELINE_UTILS.isPipelineSearchQueryable(pipeline)).to.equal(
+        true,
+      );
     });
 
     it('should return false for a pipeline with an unsupported stage', function () {
       const pipeline: Document[] = [{ $group: { _id: '$field' } }];
-      expect(isPipelineSearchQueryable(pipeline)).to.equal(false);
+      expect(VIEW_PIPELINE_UTILS.isPipelineSearchQueryable(pipeline)).to.equal(
+        false,
+      );
     });
 
     it('should return false for a $match stage without $expr', function () {
       const pipeline: Document[] = [{ $match: { nonExprKey: 'someValue' } }];
-      expect(isPipelineSearchQueryable(pipeline)).to.equal(false);
+      expect(VIEW_PIPELINE_UTILS.isPipelineSearchQueryable(pipeline)).to.equal(
+        false,
+      );
     });
 
     it('should return false for a $match stage with $expr and additional fields', function () {
@@ -44,12 +50,16 @@ describe('views', function () {
           },
         },
       ];
-      expect(isPipelineSearchQueryable(pipeline)).to.equal(false);
+      expect(VIEW_PIPELINE_UTILS.isPipelineSearchQueryable(pipeline)).to.equal(
+        false,
+      );
     });
 
     it('should return true for an empty pipeline', function () {
       const pipeline: Document[] = [];
-      expect(isPipelineSearchQueryable(pipeline)).to.equal(true);
+      expect(VIEW_PIPELINE_UTILS.isPipelineSearchQueryable(pipeline)).to.equal(
+        true,
+      );
     });
 
     it('should return false if any stage in the pipeline is invalid', function () {
@@ -58,7 +68,9 @@ describe('views', function () {
         { $match: { $expr: { $eq: ['$field', 'value'] } } },
         { $group: { _id: '$field' } },
       ];
-      expect(isPipelineSearchQueryable(pipeline)).to.equal(false);
+      expect(VIEW_PIPELINE_UTILS.isPipelineSearchQueryable(pipeline)).to.equal(
+        false,
+      );
     });
 
     it('should handle a pipeline with multiple valid stages', function () {
@@ -67,63 +79,97 @@ describe('views', function () {
         { $match: { $expr: { $eq: ['$field', 'value'] } } },
         { $set: { field2: 'value2' } },
       ];
-      expect(isPipelineSearchQueryable(pipeline)).to.equal(true);
+      expect(VIEW_PIPELINE_UTILS.isPipelineSearchQueryable(pipeline)).to.equal(
+        true,
+      );
     });
 
     it('should return false for a $match stage with no conditions', function () {
       const pipeline: Document[] = [{ $match: {} }];
-      expect(isPipelineSearchQueryable(pipeline)).to.equal(false);
+      expect(VIEW_PIPELINE_UTILS.isPipelineSearchQueryable(pipeline)).to.equal(
+        false,
+      );
     });
   });
 
   describe('isVersionSearchCompatibleForViewsDataExplorer', function () {
     it('should return true for a version greater than or equal to 8.0.0', function () {
-      expect(isVersionSearchCompatibleForViewsDataExplorer('8.0.0')).to.equal(
-        true,
-      );
-      expect(isVersionSearchCompatibleForViewsDataExplorer('8.0.1')).to.equal(
-        true,
-      );
-      expect(isVersionSearchCompatibleForViewsDataExplorer('8.1.0')).to.equal(
-        true,
-      );
+      expect(
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsDataExplorer(
+          '8.0.0',
+        ),
+      ).to.equal(true);
+      expect(
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsDataExplorer(
+          '8.0.1',
+        ),
+      ).to.equal(true);
+      expect(
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsDataExplorer(
+          '8.1.0',
+        ),
+      ).to.equal(true);
     });
 
     it('should return false for a version less than 8.0.0', function () {
-      expect(isVersionSearchCompatibleForViewsDataExplorer('7.9.9')).to.equal(
-        false,
-      );
-      expect(isVersionSearchCompatibleForViewsDataExplorer('7.0.0')).to.equal(
-        false,
-      );
+      expect(
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsDataExplorer(
+          '7.9.9',
+        ),
+      ).to.equal(false);
+      expect(
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsDataExplorer(
+          '7.0.0',
+        ),
+      ).to.equal(false);
     });
 
     it('should handle invalid version format by returning false', function () {
       expect(
-        isVersionSearchCompatibleForViewsDataExplorer('invalid-version'),
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsDataExplorer(
+          'invalid-version',
+        ),
       ).to.equal(false);
-      expect(isVersionSearchCompatibleForViewsDataExplorer('')).to.equal(false);
+      expect(
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsDataExplorer(''),
+      ).to.equal(false);
     });
   });
 
   describe('isVersionSearchCompatibleForViewsCompass', function () {
     it('should return true for a version greater than or equal to 8.1.0', function () {
-      expect(isVersionSearchCompatibleForViewsCompass('8.1.0')).to.equal(true);
-      expect(isVersionSearchCompatibleForViewsCompass('8.1.1')).to.equal(true);
-      expect(isVersionSearchCompatibleForViewsCompass('8.2.0')).to.equal(true);
+      expect(
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsCompass('8.1.0'),
+      ).to.equal(true);
+      expect(
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsCompass('8.1.1'),
+      ).to.equal(true);
+      expect(
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsCompass('8.2.0'),
+      ).to.equal(true);
     });
 
     it('should return false for a version less than 8.1.0', function () {
-      expect(isVersionSearchCompatibleForViewsCompass('8.0.9')).to.equal(false);
-      expect(isVersionSearchCompatibleForViewsCompass('8.0.0')).to.equal(false);
-      expect(isVersionSearchCompatibleForViewsCompass('7.9.9')).to.equal(false);
+      expect(
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsCompass('8.0.9'),
+      ).to.equal(false);
+      expect(
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsCompass('8.0.0'),
+      ).to.equal(false);
+      expect(
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsCompass('7.9.9'),
+      ).to.equal(false);
     });
 
     it('should handle invalid version format by returning false', function () {
       expect(
-        isVersionSearchCompatibleForViewsCompass('invalid-version'),
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsCompass(
+          'invalid-version',
+        ),
       ).to.equal(false);
-      expect(isVersionSearchCompatibleForViewsCompass('')).to.equal(false);
+      expect(
+        VIEW_PIPELINE_UTILS.isVersionSearchCompatibleForViewsCompass(''),
+      ).to.equal(false);
     });
   });
 });
