d69f6e88c27953b5518ce2b2eea77c7188ab62cc
Rhys Howell <rhys.howell@mongodb.com>
2025-11-13 14:50:12 -0800

    fixup: update meta field naming for bson autocomplete

--- packages/shell-bson-parser/src/scope.ts
+++ packages/shell-bson-parser/src/scope.ts
@@ -42,7 +42,7 @@ const SCOPE_ANY: { [x: string]: Function } = lookupMap({
     return new bson.Binary(buffer, subType);
   },
 
-  // Legacy UUID functions are from originally taken from
+  // Legacy UUID functions from
   // https://github.com/mongodb/mongo-csharp-driver/blob/ac2b2a61c6b7a193cf0266dfb8c65f86c2bf7572/uuidhelpers.js
   LegacyJavaUUID: function (u: any) {
     if (u === undefined) {
