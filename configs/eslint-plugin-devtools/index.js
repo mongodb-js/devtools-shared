e31e917e83d4f6f95c273ecb45d2ce7333f2bbb5
Anna Henningsen <anna@addaleax.net>
2025-12-15 16:17:53 +0100

    feat(eslint-plugin-devtools): add rule forbidding non-null prototypes for `Record`s
    `Record` types in TS are typically used to indicate that an object
    may have arbitrary keys. This means that using `{}` or similar object
    literals to initialize them is a poor choice -- if user code can
    cause keys like `hasOwnProperty` or `__proto__` to be used as
    keys on those objects, they will not behave as intended.
    
    This has been the cause of many security issues in the JS
    ecosystem, so let's add a lint rule to limit impact
    of these issues and force ourselves to stick to best practices.

--- configs/eslint-plugin-devtools/index.js
+++ configs/eslint-plugin-devtools/index.js
@@ -2,5 +2,6 @@
 module.exports = {
   rules: {
     'no-expect-method-without-call': require('./rules/no-expect-method-without-call'),
+    'no-plain-object-records': require('./rules/no-plain-object-records'),
   },
 };
