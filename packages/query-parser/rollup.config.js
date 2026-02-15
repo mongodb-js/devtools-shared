c36847ac58039003f7c500de9571a8ca6f2c59ad
rtritto <ariele.1994@gmail.com>
2026-02-15 14:19:04 +0100

    build(query-parser): change TSC + rollup to rollup COMPASS-10271

--- packages/query-parser/rollup.config.js
+++ packages/query-parser/rollup.config.js
@@ -1,13 +1,35 @@
 const typescript = require('@rollup/plugin-typescript');
 
-module.exports = {
-  input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'esm',
-    entryFileNames: '[name].mjs',
-    sourcemap: true,
-    preserveModules: true,
+module.exports = [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        dir: 'dist',
+        format: 'esm',
+        entryFileNames: '[name].mjs',
+        sourcemap: true,
+        preserveModules: true,
+      },
+    ],
+    plugins: [typescript()],
   },
-  plugins: [typescript()],
-};
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        dir: 'dist',
+        format: 'cjs',
+        sourcemap: true,
+        preserveModules: true,
+        exports: 'named',
+      },
+    ],
+    plugins: [
+      typescript({
+        declaration: false,
+        declarationMap: false,
+      }),
+    ],
+  },
+];
