40879b826258cd0ff447723a14078397739b4a4d
Steven Silvester <steve.silvester@mongodb.com>
2026-02-18 06:50:18 -0600

    fix(mongodb-runner): configsvr must always use 127.0.0.1 DRIVERS-3335 (#620)

--- packages/mongodb-runner/src/mongoserver.ts
+++ packages/mongodb-runner/src/mongoserver.ts
@@ -212,7 +212,7 @@ export class MongoServer extends EventEmitter<MongoServerEvents> {
     srv.isArbiter = !!options.isArbiter;
     srv.isMongos = options.binary === 'mongos';
     srv.isConfigSvr = !!options.args?.includes('--configsvr');
-    if (options.host) {
+    if (options.host && !srv.isConfigSvr) {
       srv.host = options.host;
     }
     const keyFilePath = getKeyFileOption(options.args);
