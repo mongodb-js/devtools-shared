0a5d7e4fee75bdeebd9ea572d852b282c510439e
Edjan Michiles <edjan.michiles@mongodb.com>
2026-09-15 12:05:51 +0100

    Apply suggestion from @esvm

--- packages/monorepo-tools/src/utils/get-packages-in-topological-order.ts
+++ packages/monorepo-tools/src/utils/get-packages-in-topological-order.ts
@@ -21,7 +21,7 @@ export async function getPackagesInTopologicalOrder(
 ): Promise<PackageInfo[]> {
   const patterns: string[] = JSON.parse(
     await fs.readFile(path.join(monorepoRoot, `package.json`), 'utf8'),
-  ).workspaces || ['.']; // <---- default value that considers monorepo root as a "workspace" for further analysis
+  ).workspaces || ['.']; // default value that considers monorepo root as a "workspace" for further analysis
 
   const packageJsonPaths = await glob(
     // NOTE: glob patterns should always use forward slashes,
